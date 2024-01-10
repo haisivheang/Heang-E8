724c743642125085b1d215c1a94a53c5c80a12f0
haisivheang <haisivheang@gmail.com>
2024-01-10 20:10:41 +0700

    02commit

--- src/Midterm02.java
+++ src/Midterm02.java
@@ -1,25 +1,40 @@
-public class Midterm02 {
-    class Rectangle {
-        private double width;
-        private double length;
+package demo;
 
-        public Rectangle(double width, double length) {
-            this.width = width;
-            this.length = length;
-        }
+public class Main {
+    public static void main(String[] args) {
+        // Example usage
+        Rectangle rectangle = new Rectangle(3, 5);
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
 
-        public double getArea() {
-            return width * length;
-        }
+        Square square = new Square(4);
+        System.out.println("Square Area: " + square.getArea()); // Inherits getArea() from Rectangle
+        System.out.println("Square Perimeter: " + square.getPerimeter()); // Inherits getPerimeter() from Rectangle
+    }
+}
+package demo;
 
-        public double getPerimeter() {
-            return 2 * (width + length);
-        }
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
     }
 
-    class Square extends Rectangle {
-        public Square(double side) {
-            super(side, side);
-        }
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+}
+package demo;
+
+public class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
     }
 }
